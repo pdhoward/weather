13fc51a5705d2f232d93233df9e3a14ee24cf37a
pdhoward <patrick.howard@hotmail.com>
2019-05-16 10:55:07 -0400

    component update

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -4,11 +4,16 @@
     <ul>
       <li v-for="child in childComponents">{{ child }}</li>
     </ul>
+      <div id="pagecontent">      
+      {{ weather_data }}
+
+      <li v-for="x in weather_data">{{ x }}</li>   </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["weather_data"],
   data () {
     return {
       childComponents: ['TempVarChart.vue', 'Highlights.vue']
